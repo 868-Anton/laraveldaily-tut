7940444d30922cbdf57360f56fb82833a1321b6a
= <anton.graham.2011@gmail.com>
2022-10-12 04:19:26 -0400

    create PostController

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //
+
+    public function show(Post $post)
+    {
+
+        return view('post',compact('post'));
+    }
+}
