e47d321dd759f3bc98d85deb8a94d83d198831c4
= <anton.graham.2011@gmail.com>
2022-10-12 03:22:14 -0400

    gets all posts and posts by categories

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,15 +8,15 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    //gets all posts and posts by categories 
     public function index(){
 
     $categories = Category::all();
     $posts = Post::when(request('category_id'), function($query){
-                        $query->where('category_id', request('category_id'));
-    })
-    ->latest()
-    ->get();
+            $query->where('category_id', request('category_id'));
+        })
+        ->latest()
+        ->get();
 
     return view('index',
     compact('categories','posts'));
